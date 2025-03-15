732a10c1664d7f62f1c5181e3c404f512419514c
cabsjonel@gmail.com <cabsjonel@gmail.com>
2025-03-15 23:32:44 +0800

    main : Adde project folders

---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+// Define an interface for User
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model<IUser>("User", UserSchema);
